a89129b0ac7ae3b54d67dfbfdf2e909ffca26eb6
Sadiya-sheik <83219444+Sadiya-sheik@users.noreply.github.com>
2021-06-01 17:04:59 +0530

    Update acertus-ecs-cdk.ts

--- bin/acertus-ecs-cdk.ts
+++ bin/acertus-ecs-cdk.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { AcertusEcsCdkStack } from '../lib/acertus-ecs-cdk-stack';
+import { AcertusEcsCdkStacksample } from '../lib/acertus-ecs-cdk-stack';
 
 const app = new cdk.App();
-new AcertusEcsCdkStack(app, 'AcertusEcsCdkStack', {
+new AcertusEcsCdkStacksample(app, 'AcertusEcsCdkStacksample', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
