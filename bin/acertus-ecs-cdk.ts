d09a9717decdc453579a16b13ee25746553aee3f
Sadiya-sheik <83219444+Sadiya-sheik@users.noreply.github.com>
2021-06-02 21:45:48 +0530

    Update acertus-ecs-cdk.ts

--- bin/acertus-ecs-cdk.ts
+++ bin/acertus-ecs-cdk.ts
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { AcertusEcsCdkStacksample } from '../lib/acertus-ecs-cdk-stack';
+import { AcertusEcsCdkStackfinal } from '../lib/acertus-ecs-cdk-stack';
 
 const app = new cdk.App();
 const envUS  = { account: '665106695518', region: 'ap-south-1' };
-new AcertusEcsCdkStacksample(app, 'AcertusEcsCdkStacksample', { env: envUS });
+new AcertusEcsCdkStackfinal(app, 'AcertusEcsCdkStackfinal', { env: envUS });
